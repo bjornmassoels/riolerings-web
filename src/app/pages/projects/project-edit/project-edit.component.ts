8f4a6d918636bf6e0dc8c68d48789584f5ce872a
bjorn <bjornlain2@outlook.com>
2024-05-20 17:21:08 +0200

    ff

--- src/app/pages/projects/project-edit/project-edit.component.ts
+++ src/app/pages/projects/project-edit/project-edit.component.ts
@@ -108,7 +108,6 @@ export class ProjectEditComponent implements OnInit,OnDestroy {
       } else {
         this.hasPreviousPage = true;
       }
-      console.log(this.currentProject.droogWaterAfvoer.reductieAchter)
       this.usersWhoEdited = '';
       this._id = this.route.snapshot.paramMap.get('id');
       this.loadData();
